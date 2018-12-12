ad0d5fc15124443f07701c9bf07a999f5d5fe366
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-11 23:30:42 -0500

    Add a couple of timezone controllers

---
+++ src/DevChatter.DevStreams.Web/Controllers/CountriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Globalization;
+using TimeZoneNames;
+
+namespace DevChatter.DevStreams.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : Controller
+    {
+        [HttpGet]
+        public IDictionary<string, string> Get()
+        {
+            return TZNames.GetCountryNames(CultureInfo.CurrentUICulture.Name);
+        }
+    }
+}
---
+++ src/DevChatter.DevStreams.Web/Controllers/TimeZonesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using TimeZoneNames;
+
+namespace DevChatter.DevStreams.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class TimeZonesController : Controller
+    {
+        [HttpGet]
+        public IDictionary<string, string> Get(string country, DateTimeOffset? threshold)
+        {
+            var languageCode = CultureInfo.CurrentUICulture.Name;
+
+            if (country != null)
+            {
+                return GetTimeZonesForCountry(country, threshold, languageCode);
+            }
+
+            return TZNames.GetCountryNames(languageCode)
+                .SelectMany(x => GetTimeZonesForCountry(x.Key, threshold, languageCode)
+                    .Select(y => new { CountryCode = x.Key, Country = x.Value, TimeZoneId = y.Key, TimeZoneName = y.Value }))
+                .GroupBy(x => x.TimeZoneId)
+                .ToDictionary(x => x.Key, x => $"{x.First().Country} - {x.First().TimeZoneName}");
+        }
+
+        private static IDictionary<string, string> GetTimeZonesForCountry(string country, DateTimeOffset? threshold, string languageCode)
+        {
+            return threshold == null
+                ? TZNames.GetTimeZonesForCountry(country, languageCode)
+                : TZNames.GetTimeZonesForCountry(country, languageCode, threshold.Value);
+        }
+    }
+}

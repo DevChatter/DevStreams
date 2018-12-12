419eeeac93925fccd438e33ba5ba46981705f326
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-11 23:15:13 -0500

    Move Model mapping to ViewModel

--- src/DevChatter.DevStreams.Web/Data/ViewModel/CreateStreamTimeViewModel.cs
+++ src/DevChatter.DevStreams.Web/Data/ViewModel/CreateStreamTimeViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using DevChatter.DevStreams.Web.Data.Model;
 using NodaTime;
+using NodaTime.Text;
 
 namespace DevChatter.DevStreams.Web.Data.ViewModel
 {
     public class CreateStreamTimeViewModel
     {
+        private static readonly LocalTimePattern TimePattern =
+            LocalTimePattern.CreateWithInvariantCulture("HH:mm");
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please choose your timezone.")]
         public string TimeZoneId { get; set; }
 
@@ -13,5 +18,18 @@ namespace DevChatter.DevStreams.Web.Data.ViewModel
 
         [Required]
         public string LocalTime { get; set; }
+
+        public StreamTime ToModel()
+        {
+            var parseResult = TimePattern.Parse(LocalTime);
+            var localTime = parseResult.Value;
+
+            return new StreamTime
+            {
+                DayOfWeek = DayOfWeek,
+                TimeZoneId = TimeZoneId,
+                LocalTime = localTime
+            };
+        }
     }
 }
--- src/DevChatter.DevStreams.Web/Pages/Streams/Create.cshtml.cs
+++ src/DevChatter.DevStreams.Web/Pages/Streams/Create.cshtml.cs
@@ -1,20 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using DevChatter.DevStreams.Web.Data.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using NodaTime;
 using NodaTime.Extensions;
 using NodaTime.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using DevChatter.DevStreams.Web.Data.ViewModel;
-using Microsoft.AspNetCore.Mvc;
 
 namespace DevChatter.DevStreams.Web.Pages.Streams
 {
     public class CreateModel : PageModel
     {
-        private static readonly LocalTimePattern TimePattern = 
-            LocalTimePattern.CreateWithInvariantCulture("HH:mm");
         public static readonly ZonedDateTimePattern ZonedDateTimePattern = 
             ZonedDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm o<G>", null);
 
@@ -25,7 +22,6 @@ namespace DevChatter.DevStreams.Web.Pages.Streams
         {
             _clock = SystemClock.Instance;
         }
-
 
         [BindProperty]
         public CreateStreamTimeViewModel StreamTime { get; set; } = new CreateStreamTimeViewModel
@@ -44,28 +40,27 @@ namespace DevChatter.DevStreams.Web.Pages.Streams
             // TODO: Use better names than the ID itself.
             TimeZoneIds = tzdbIds.Select(x => new SelectListItem(x, x));
 
-            var localTime = new LocalTime(14,0,0,0);
-            string formatted = LocalTimePattern.ExtendedIso.Format(localTime);
+            //var localTime = new LocalTime(14,0,0,0);
+            //string formatted = LocalTimePattern.ExtendedIso.Format(localTime);
 
-            var parsed = LocalTimePattern.ExtendedIso.Parse(formatted).Value;
+            //var parsed = LocalTimePattern.ExtendedIso.Parse(formatted).Value;
         }
 
         public void OnPost()
         {
-            var parseResult = TimePattern.Parse(StreamTime.LocalTime);
-            var localTime = parseResult.Value;
+            var streamTime = StreamTime.ToModel();
 
-            var zone = DateTimeZoneProviders.Tzdb[StreamTime.TimeZoneId];
+            var zone = DateTimeZoneProviders.Tzdb[streamTime.TimeZoneId];
             //var version = DateTimeZoneProviders.Tzdb.VersionId;
             var zonedClock = _clock.InZone(zone);
 
             LocalDate today = zonedClock.GetCurrentDate();
-            LocalDate next = today.With(DateAdjusters.Next(StreamTime.DayOfWeek));
+            LocalDate next = today.With(DateAdjusters.Next(streamTime.DayOfWeek));
 
             SuggestedSessions = new List<ZonedDateTime>();
             for (int i = 0; i < 52; i++)
             {
-                LocalDateTime nextLocalDateTime = next + localTime;
+                LocalDateTime nextLocalDateTime = next + streamTime.LocalTime;
 
                 SuggestedSessions.Add(nextLocalDateTime.InZoneLeniently(zone));
                 next = next.PlusWeeks(1);

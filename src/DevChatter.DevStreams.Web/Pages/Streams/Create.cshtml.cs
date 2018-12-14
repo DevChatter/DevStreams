9ca68d4dc345560ff7ad9f926f9baa0c80206ce5
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-14 15:32:56 -0500

    Create CRUD pages for Channel

---
+++ src/DevChatter.DevStreams.Web/Pages/Streams/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DevChatter.DevStreams.Core.Model;
+using DevChatter.DevStreams.Web.Data;
+
+namespace DevChatter.DevStreams.Web.Pages.Streams
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DevChatter.DevStreams.Web.Data.ApplicationDbContext _context;
+
+        public CreateModel(DevChatter.DevStreams.Web.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Channel Channel { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Channels.Add(Channel);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/DevChatter.DevStreams.Web/Pages/Streams/Delete.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using DevChatter.DevStreams.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DevChatter.DevStreams.Web.Pages.Streams
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DevChatter.DevStreams.Web.Data.ApplicationDbContext _context;
+
+        public DeleteModel(DevChatter.DevStreams.Web.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Channel Channel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Channel = await _context.Channels.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Channel == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Channel = await _context.Channels.FindAsync(id);
+
+            if (Channel != null)
+            {
+                _context.Channels.Remove(Channel);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

bae9a89f7b0c1f445a137d8e372803caa704b390
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-14 13:16:07 -0500

    Wiring up vuejs timezone cascade

---
+++ src/DevChatter.DevStreams.Web/wwwroot/js/vue/streams-create.js
@@ -0,0 +1,26 @@
+﻿let app = new Vue({
+    el: "#app",
+    data: {
+        selectedCountry: null,
+        selectedTimeZone: null,
+        timeZoneOptions: [],
+    },
+    watch: {
+        selectedCountry: function (value, previous) {
+            this.timeZoneOptions = [];
+            this.selectedTimeZone = null;
+            this.fetchTimeZones(value);
+        }
+    },
+    methods: {
+        fetchTimeZones: function (countryCode) {
+            axios.get(`/api/TimeZones/?CountryCode=${countryCode}`)
+                .then(response => {
+                    this.timeZoneOptions = Object.keys(response.data)
+                        .map(key => ({ key: key, value: response.data[key] }));
+                }, error => {
+                    console.log(error.statusText);
+                });
+        }
+    }
+});

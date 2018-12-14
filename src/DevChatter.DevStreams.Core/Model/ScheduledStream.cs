524ce7b61bb2aa2d7ebc1f8dd4631e78823d0910
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-14 14:23:51 -0500

    remove channel reference from ScheduledStream

--- src/DevChatter.DevStreams.Core/Model/ScheduledStream.cs
+++ src/DevChatter.DevStreams.Core/Model/ScheduledStream.cs
@@ -8,7 +8,6 @@ namespace DevChatter.DevStreams.Core.Model
     /// </summary>
     public class ScheduledStream : DataEntity
     {
-        public Channel Channel { get; set; }
         public string TimeZoneId { get; set; }
         public IsoDayOfWeek DayOfWeek { get; set; }
         public LocalTime LocalStartTime { get; set; }

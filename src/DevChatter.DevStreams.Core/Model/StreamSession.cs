24bce482a5199821a50c1aeb9aa401b6927e9bcd
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-14 12:16:11 -0500

    Start and End times on StreamSession

--- src/DevChatter.DevStreams.Core/Model/StreamSession.cs
+++ src/DevChatter.DevStreams.Core/Model/StreamSession.cs
@@ -4,9 +4,11 @@ namespace DevChatter.DevStreams.Core.Model
 {
     public class StreamSession
     {
-        public LocalDateTime LocalDateTime { get; set; }
+        public LocalDateTime LocalStartDateTime { get; set; }
+        public LocalDateTime LocalEndDateTime { get; set; }
         public string TimeZoneId { get; set; }
-        public Instant UtcInstant { get; set; }
+        public Instant UtcStartTime { get; set; }
+        public Instant UtcEndTime { get; set; }
         public string TzdbVersionId { get; set; }
     }
 }

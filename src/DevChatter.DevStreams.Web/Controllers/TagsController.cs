7a3853ef7db7b0fc7394390775d67b0dda664a09
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-01-24 12:07:31 -0500

    reformatting code

--- src/DevChatter.DevStreams.Web/Controllers/TagsController.cs
+++ src/DevChatter.DevStreams.Web/Controllers/TagsController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DevChatter.DevStreams.Web.Data;
+﻿using DevChatter.DevStreams.Web.Data;
 using DevChatter.DevStreams.Web.Data.ViewModel.Tags;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DevChatter.DevStreams.Web.Controllers
 {
@@ -22,9 +21,14 @@ namespace DevChatter.DevStreams.Web.Controllers
         [HttpGet]
         public async Task<IList<TagViewModel>> Get(string filter)
         {
-            var models = await _dbContext.Tags.Where(t => t.Name.Contains(filter)).ToListAsync();
+            var models = await _dbContext.Tags
+                .Where(t => t.Name.Contains(filter)) // TODO: Much better filtering
+                .ToListAsync();
 
-            List<TagViewModel> viewModels = models.Select(x => x.ToViewModel()).ToList();
+            var viewModels = models
+                .Select(x => x.ToViewModel())
+                .ToList();
+
             return viewModels;
         }
     }

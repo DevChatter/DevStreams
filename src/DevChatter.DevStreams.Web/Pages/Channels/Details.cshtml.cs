f82310220a01f51fb08e260bff8e0900005f86fd
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-05 15:39:40 -0500

    Adding channel details page

---
+++ src/DevChatter.DevStreams.Web/Pages/Channels/Details.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevChatter.DevStreams.Web.Pages.Channels
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet(int id)
+        {
+            // TODO: Load the details of this channel
+        }
+    }
+}

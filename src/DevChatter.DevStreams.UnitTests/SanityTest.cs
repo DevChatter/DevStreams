c05c71763836ca9b69d8ffc1e5a16107fef8557e
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-07 13:33:44 -0500

    Adding the SanityTest

---
+++ src/DevChatter.DevStreams.UnitTests/SanityTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DevChatter.DevStreams.UnitTests
+{
+    public class SanityTest
+    {
+        [Fact]
+        public void TrueShouldBeTrue()
+        {
+            const bool truth = true;
+            truth.Should().BeTrue();
+        }
+    }
+}

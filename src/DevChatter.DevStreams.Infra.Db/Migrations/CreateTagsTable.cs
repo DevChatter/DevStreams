90e7710b0051cd527a4b0112df232a2d41fbc4df
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-19 15:27:32 -0500

    Adding Initial FluentMigrator Migrations

---
+++ src/DevChatter.DevStreams.Infra.Db/Migrations/CreateTagsTable.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace DevChatter.DevStreams.Infra.Db.Migrations
+{
+    [Migration(201902192023)]
+    public class CreateTagsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Tags")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(255)
+                .WithColumn("Description").AsString(255).NotNullable();
+
+            Create.Index("ix_Tags_Name")
+                .OnTable("Tags")
+                .OnColumn("Name")
+                .Ascending()
+                .WithOptions().Unique()
+                .WithOptions().NonClustered();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Tags");
+        }
+    }
+}

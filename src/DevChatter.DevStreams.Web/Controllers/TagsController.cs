24f0576c34513b4f66913b970a19f64bdf1613b6
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-20 17:29:05 -0500

    Remove EF Core from TagsController

--- src/DevChatter.DevStreams.Web/Controllers/TagsController.cs
+++ src/DevChatter.DevStreams.Web/Controllers/TagsController.cs
@@ -1,24 +1,21 @@
-﻿using DevChatter.DevStreams.Web.Data;
+﻿using DevChatter.DevStreams.Core.Model;
+using DevChatter.DevStreams.Core.Services;
+using DevChatter.DevStreams.Web.Data;
 using DevChatter.DevStreams.Web.Data.ViewModel.Tags;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DevChatter.DevStreams.Core.Model;
-using DevChatter.DevStreams.Core.Services;
 
 namespace DevChatter.DevStreams.Web.Controllers
 {
     [Route("api/[controller]")]
     public class TagsController : Controller
     {
-        private readonly ApplicationDbContext _dbContext;
         private readonly ITagSearchService _tagSearchService;
 
-        public TagsController(ApplicationDbContext dbContext, ITagSearchService tagSearchService)
+        public TagsController(ITagSearchService tagSearchService)
         {
-            _dbContext = dbContext;
             _tagSearchService = tagSearchService;
         }
 
@@ -26,10 +23,6 @@ namespace DevChatter.DevStreams.Web.Controllers
         public async Task<IList<TagViewModel>> Get(string filter)
         {
             List<Tag> models = await _tagSearchService.Find(filter);
-
-            //List<Tag> models = await _dbContext.Tags
-            //    .Where(t => t.Name.Contains(filter)) // TODO: Much better filtering
-            //    .ToListAsync();
 
             var viewModels = models
                 .Select(x => x.ToViewModel())

89e69c9a1cc268a9ce8824ca1c2022ec6a0ecdf5
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-22 20:08:40 -0500

    Include DevChatter in the database
    This will provide initial data. We'll need to alter the row with an associated account once we have auth in place.

---
+++ src/DevChatter.DevStreams.Infra.Db/Migrations/InsertDevChatterChannel.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using FluentMigrator;
+
+namespace DevChatter.DevStreams.Infra.Db.Migrations
+{
+    [Migration(201902230059)]
+    public class InsertDevChatterChannel : Migration
+    {
+        public override void Up()
+        {
+            var channel = new
+            { 
+                Name = "DevChatter", 
+                Uri = "https://www.twitch.tv/devchatter", 
+                CountryCode = "US", 
+                TimeZoneId = "America/New_York" 
+            };
+            Insert.IntoTable("Channels").Row(channel);
+        }
+
+        public override void Down()
+        {
+            Delete.FromTable("Channels").Row(new { Name = "DevChatter" });
+        }
+    }
+}

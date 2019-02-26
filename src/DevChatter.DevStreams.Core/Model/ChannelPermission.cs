aab0936b31350168879fd6d1a4ba1a6b43f74b2a
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-02-26 15:51:33 -0500

    Add ChannelPermissions Table

---
+++ src/DevChatter.DevStreams.Core/Model/ChannelPermission.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevChatter.DevStreams.Core.Model
+{
+    [Table("ChannelPermissions")]
+    public class ChannelPermission
+    {
+        public int ChannelId { get; set; }
+        public string UserId { get; set; }
+        public ChannelRole ChannelRole { get; set; }
+    }
+}

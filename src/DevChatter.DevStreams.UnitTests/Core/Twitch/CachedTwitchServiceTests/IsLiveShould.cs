ba9452ca9c2debcff576620e268c0bf9d838ed7f
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-03-26 15:39:00 -0400

    WIP - switch from Fakes to Moq

--- src/DevChatter.DevStreams.UnitTests/Core/Twitch/CachedTwitchServiceTests/IsLiveShould.cs
+++ src/DevChatter.DevStreams.UnitTests/Core/Twitch/CachedTwitchServiceTests/IsLiveShould.cs
@@ -5,63 +5,33 @@ using System.Threading.Tasks;
 using DevChatter.DevStreams.Core.Caching;
 using FluentAssertions;
 using Xunit;
+using Moq;
 
 namespace DevChatter.DevStreams.UnitTests.Core.Twitch.CachedTwitchServiceTests
 {
     public class IsLiveShould
     {
+        private readonly Mock<ICacheLayer<int, bool>> _cacheMock;
+        private readonly Mock<ITwitchService> _twitchMock;
         const int twitchId = 1337;
 
         public IsLiveShould()
         {
+            _cacheMock = new Mock<ICacheLayer<int,bool>>();
+            _twitchMock = new Mock<ITwitchService>();
         }
 
         [Fact]
         public void NotCallTwitch_WhenValueIsCached()
         {
-            var fakeService = new FakeTwitchService();
-            var fakeCacheLayer = new FakeCacheLayer {Cache = {[twitchId] = true}};
-            var twitchService = new CachedTwitchService(fakeService, fakeCacheLayer);
+            _cacheMock.Setup(x => 
+                    x.GetValueOrFallback(twitchId, It.IsAny<Func<int, Task<bool>>>()))
+                .Returns(Task.FromResult(true));
+            var twitchService = new CachedTwitchService(_twitchMock.Object, _cacheMock.Object);
 
             bool isLive = twitchService.IsLive(twitchId).Result;
 
-            fakeService.IsLiveCalled.Should().BeFalse();
+            _twitchMock.Verify(x => x.IsLive(twitchId), Times.Never);
         }
-
-    }
-
-    public class FakeCacheLayer : ICacheLayer<int,bool>
-    {
-        public Dictionary<int,bool> Cache { get; set; } = new Dictionary<int, bool>();
-        public async Task<bool> GetValueOrFallback(int key, Func<int, Task<bool>> fallBack)
-        {
-            if (Cache.TryGetValue(key, out bool value))
-            {
-                return value;
-            }
-
-            return await fallBack.Invoke(key);
-        }
-    }
-
-    public class FakeTwitchService : ITwitchService
-    {
-        public Task<List<string>> GetChannelIds(List<string> channelNames)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<List<string>> GetLiveChannels(List<string> channelNames)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> IsLive(int twitchId)
-        {
-            IsLiveCalled = true;
-            return Task.FromResult(true);
-        }
-
-        public bool IsLiveCalled { get; set; } = false;
     }
 }

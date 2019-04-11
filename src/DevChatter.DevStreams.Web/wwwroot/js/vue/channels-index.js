980e846196fd3b0bcaf6541c89b220294811a2a4
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-04-11 14:01:05 -0400

    Loading channel data from vue

---
+++ src/DevChatter.DevStreams.Web/wwwroot/js/vue/channels-index.js
@@ -0,0 +1,47 @@
+﻿let app = new Vue({
+    el: "#channelList",
+    data: {
+        isLoadingData: true,
+        channels: [],
+        searchFilter: ''
+    },
+    mounted() {
+        this.fetchChannels();
+    },
+    methods: {
+        fetchChannels: async function () {
+            const res = await axios.post('/graphql',
+                {
+                    query: `query getChannels
+                        { channels
+                            { id name
+                                tags
+                                {
+                                    name
+                                }
+                                nextStream
+                                {
+                                    utcStartTime 
+                                    utcEndTime
+                                }
+                            }
+                        }`
+                });
+            this.channels = res.data.data.channels;
+            this.isLoadingData = false;
+        },
+        formatTags: function (tags) {
+            if (tags) {
+                return tags.map(tag => tag.name).join(', ');
+            }
+            return "-";
+        },
+        formatStartTime: function (streamSession) {
+            if (streamSession) {
+                // TODO: Show in correct time zone.
+                return streamSession.utcStartTime;
+            }
+            return "None Scheduled";
+        }
+    }
+});

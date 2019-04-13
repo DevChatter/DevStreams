11188d1b1d0c068e63634814f0ee8a96111f688b
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-04-13 14:50:56 -0400

    Change GetChannelsByTagIds Query to require all Tags match

--- src/DevChatter.DevStreams.Infra.Dapper/DapperChannelRepository.cs
+++ src/DevChatter.DevStreams.Infra.Dapper/DapperChannelRepository.cs
@@ -3,6 +3,7 @@ using DevChatter.DevStreams.Core.Model;
 using DevChatter.DevStreams.Core.Settings;
 using Microsoft.Extensions.Options;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DevChatter.DevStreams.Infra.Dapper
@@ -16,19 +17,25 @@ namespace DevChatter.DevStreams.Infra.Dapper
         }
 
         /// <summary>
-        /// 
+        /// Get Channels By Tag IDs. Channel must have all tags.
         /// </summary>
-        /// <param name="tagIds"></param>
-        /// <returns></returns>
+        /// <param name="tagIds">Array of tag ids.</param>
+        /// <returns>List of Channels having all passed TagIds.</returns>
         public async Task<List<Channel>> GetChannelsByTagIds(params int[] tagIds)
         {
             const string sql = @"
-            SELECT DISTINCT c.*
-            FROM [Channels] c 
-            INNER JOIN [ChannelTags] ct ON ct.ChannelId = c.Id
-            WHERE ct.TagId in @tagIds";
+                                SELECT *
+                                FROM [Channels]
+                                WHERE Id in (
+                                    SELECT ChannelId 
+                                    FROM [ChannelTags]
+                                    WHERE TagId in @tagIds
+                                    GROUP BY ChannelId
+                                    HAVING count(*) = @tagCount
+                                )";
 
-            return await QueryAsync<Channel>(sql, new { tagIds });
+            var args = new { tagIds = tagIds, tagCount = tagIds.Length };
+            return await QueryAsync<Channel>(sql, args);
         }
     }
 }

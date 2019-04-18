8eb6fe89aec58f213bd1eb304c71f277c9ad2a68
Dom Reilly <8234099+reillydom@users.noreply.github.com>
2019-04-18 22:44:06 +0100

    Added new column for 'ImageUrl' Added migration to alter table with column on 'up' method call and drop on 'down' method call.

--- src/DevChatter.DevStreams.Core/Model/TwitchChannel.cs
+++ src/DevChatter.DevStreams.Core/Model/TwitchChannel.cs
@@ -7,5 +7,10 @@
         public string TwitchName { get; set; }
         public bool IsAffiliate { get; set; }
         public bool IsPartner { get; set; }
-    }
+
+		// DAR added 18/0/2019 -- see issue https://github.com/DevChatter/DevStreams/issues/66#issue-434758220
+		public string ImageUrl { get; set; }
+
+
+	}
 }
---
+++ src/DevChatter.DevStreams.Infra.Db/Migrations/AddChannelImageToTwitchChannel.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevChatter.DevStreams.Infra.Db.Migrations
+{
+	[Migration(201904182000)]
+	public class AddChannelImageToTwitchChannel : Migration
+	{
+		private const string ColumnName = "ImageUrl";
+		private const string TableName = "TwitchChannels";
+
+		public override void Up()
+		{
+			Alter.Table(TableName)
+				.AddColumn(ColumnName)
+				.AsString(4000) // Note: 4000 is Db max value in Fluent Nhibernate. Not sure if this is a good idea or not? -- DAR 18/04/2019
+				.WithDefaultValue("")
+				.NotNullable();
+		}
+
+
+		public override void Down()
+		{
+			Delete.Column(ColumnName)
+				.FromTable(TableName);
+		}
+
+	}
+}

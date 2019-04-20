cb8c3f94eeb379694320da066cc7dfa57a70ad03
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-04-20 13:25:57 -0400

    Limit the Soundex SQL Query to TOP 1

--- src/DevChatter.DevStreams.Infra.Dapper/Services/ChannelSearchService.cs
+++ src/DevChatter.DevStreams.Infra.Dapper/Services/ChannelSearchService.cs
@@ -1,12 +1,12 @@
 ﻿using Dapper;
 using DevChatter.DevStreams.Core.Model;
+using DevChatter.DevStreams.Core.Services;
 using DevChatter.DevStreams.Core.Settings;
 using Microsoft.Extensions.Options;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using DevChatter.DevStreams.Core.Services;
 using System.Threading.Tasks;
 
 namespace DevChatter.DevStreams.Infra.Dapper.Services
@@ -45,13 +45,13 @@ namespace DevChatter.DevStreams.Infra.Dapper.Services
 
         public async Task<Channel> GetChannelSoundex(string standardizedChannelName)
         {
-            var sql = @"SELECT * FROM Channels WHERE SOUNDEX(Name) = SOUNDEX(@standardizedChannelName) 
+            var sql = @"SELECT TOP 1 * FROM Channels WHERE SOUNDEX(Name) = SOUNDEX(@standardizedChannelName) 
                         ORDER BY DIFFERENCE(Name, @standardizedChannelName) DESC";
-            using (System.Data.IDbConnection connection = new SqlConnection(_dbSettings.DefaultConnection))
+            using (IDbConnection connection = new SqlConnection(_dbSettings.DefaultConnection))
             {
                 using (var multi = await connection.QueryMultipleAsync(sql, new { standardizedChannelName }))
                 {
-                    return (await multi.ReadAsync<Channel>()).FirstOrDefault();
+                    return (await multi.ReadAsync<Channel>()).SingleOrDefault();
                 }
             }
         }

5ee1e9aa7323bcb5eded341edb940dc87ba0ee06
Dom Reilly <8234099+reillydom@users.noreply.github.com>
2019-04-27 18:56:24 +0100

    Fixed incorrect indentation using tabs instead of spaces.

--- src/DevChatter.DevStreams.Web/Data/ViewModel/Channels/ChannelIndexModel.cs
+++ src/DevChatter.DevStreams.Web/Data/ViewModel/Channels/ChannelIndexModel.cs
@@ -2,22 +2,22 @@
 
 namespace DevChatter.DevStreams.Web.Data.ViewModel.Channels
 {
-	public class ChannelIndexModel
-	{
-		public int Id { get; set; }
+    public class ChannelIndexModel
+    {
+        public int Id { get; set; }
 
-		[Display(Name = "Name")]
-		public string Name { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
 
-		[Display(Name = "Uri")]
-		public string Uri { get; set; }
+        [Display(Name = "Uri")]
+        public string Uri { get; set; }
 
-		[Display(Name = "Scheduled Streams")]
-		public int ScheduledStreamsCount { get; set; }
+        [Display(Name = "Scheduled Streams")]
+        public int ScheduledStreamsCount { get; set; }
 
-		[Display(Name = "Time Zone")]
-		public string TimeZoneName { get; set; }
+        [Display(Name = "Time Zone")]
+        public string TimeZoneName { get; set; }
 
 
-	}
+    }
 }

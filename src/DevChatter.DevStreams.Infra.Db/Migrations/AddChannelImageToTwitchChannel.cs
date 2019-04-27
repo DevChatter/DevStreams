f70f1f3eb81ea2cb3f1392a9cfe4a6e12cd0b270
Dom Reilly <8234099+reillydom@users.noreply.github.com>
2019-04-27 18:56:24 +0100

    Fixed incorrect indentation using tabs instead of spaces.

--- src/DevChatter.DevStreams.Infra.Db/Migrations/AddChannelImageToTwitchChannel.cs
+++ src/DevChatter.DevStreams.Infra.Db/Migrations/AddChannelImageToTwitchChannel.cs
@@ -5,27 +5,27 @@ using System.Text;
 
 namespace DevChatter.DevStreams.Infra.Db.Migrations
 {
-	[Migration(201904182000)]
-	public class AddChannelImageToTwitchChannel : Migration
-	{
-		private const string ColumnName = "ImageUrl";
-		private const string TableName = "TwitchChannels";
+    [Migration(201904182000)]
+    public class AddChannelImageToTwitchChannel : Migration
+    {
+        private const string ColumnName = "ImageUrl";
+        private const string TableName = "TwitchChannels";
 
-		public override void Up()
-		{
-			Alter.Table(TableName)
-				.AddColumn(ColumnName)
-				.AsString(4000) // Note: 4000 is Db max value in Fluent Nhibernate. Not sure if this is a good idea or not? -- DAR 18/04/2019
-				.WithDefaultValue("")
-				.NotNullable();
-		}
+        public override void Up()
+        {
+            Alter.Table(TableName)
+                .AddColumn(ColumnName)
+                .AsString(4000) // Note: 4000 is Db max value in Fluent Nhibernate. Not sure if this is a good idea or not? -- DAR 18/04/2019
+                .WithDefaultValue("")
+                .NotNullable();
+        }
 
 
-		public override void Down()
-		{
-			Delete.Column(ColumnName)
-				.FromTable(TableName);
-		}
+        public override void Down()
+        {
+            Delete.Column(ColumnName)
+                .FromTable(TableName);
+        }
 
-	}
+    }
 }

cc783b8a2a03c5b1b78ae75ab94d5a8e1cf138e4
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-05-16 13:11:23 -0400

    Add Id to LiveChannelViewModel

--- src/DevChatter.DevStreams.Web/Data/ViewModel/LiveChannels/LiveChannelViewModel.cs
+++ src/DevChatter.DevStreams.Web/Data/ViewModel/LiveChannels/LiveChannelViewModel.cs
@@ -4,11 +4,11 @@ namespace DevChatter.DevStreams.Web.Data.ViewModel.LiveChannels
 {
     public class LiveChannelViewModel
     {
+        public int Id { get; set; }
         public string ChannelName { get; set; }
         public string Uri { get; set; }
         public Instant StartedAt { get; set; }
         public Duration TimeOnline => SystemClock.Instance.GetCurrentInstant() - StartedAt;
         public int ViewerCount { get; set; }
-
     }
 }
